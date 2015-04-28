1f3b574bfac26f3fb6e738b86f687d20986e7d20
Jose Cambronero <josepablocam@gmail.com>
2015-04-27 21:55:56 -0400

    working version of maximum entropy model

--- term_project/src/MaxEntModelPredict.java
+++ term_project/src/MaxEntModelPredict.java
@@ -2,10 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
-import java.io.File;
+
 
 
 import opennlp.maxent.*;
@@ -15,16 +17,34 @@ import opennlp.model.*;
 
 class MaxEntModelPredict {
     
+    //A simple decoding method that commits to a tag at time i, rather than
+    //performing a dynamic search like viterbi
     public static ArrayList<String> greedy_decode(GISModel model, ArrayList<String[]> feat_matrix)
     { 
         ArrayList<String> results = new ArrayList<String>();
+        String tag = null;
+        String prev_tag = "START";
+        String prev_tag2 = "START";
         
-        for(String[] feats : feat_matrix)
+        
+        for(String[] raw_feats : feat_matrix)
         {
-            if(feats != null)
+            if(raw_feats != null)
             {   
-                String tag = model.getBestOutcome(model.eval(feats));
+                ArrayList<String> ext_feats = new ArrayList<String>(Arrays.asList(raw_feats));
+        
+                //add 2 prior tags as features
+                ext_feats.add("tag_i-1=" + prev_tag);
+                ext_feats.add("tag_i-2=" + prev_tag2);
+                
+                //predict and store
+                String[] feats = ext_feats.toArray(new String[ext_feats.size()]);
+                tag = model.getBestOutcome(model.eval(feats));
                 results.add(tag);
+                
+                //move tags to account for i
+                prev_tag2 = prev_tag;
+                prev_tag = tag;
             }
             else
             {
@@ -36,183 +56,161 @@ class MaxEntModelPredict {
     }
     
     
-    private static String[] mod_prevBioFeat(String[] feats, String tag)
-    {
-        //modify a prevBio=null to the new tag
-        String[] mod_feats = new String[feats.length];
-    
-        
-        for(int i = 0; i < feats.length; i++)
-        {
-            String[] feat_comp = feats[i].split("=");
-            
-            if(feat_comp[0].equals("prevBIO"))
-            {
-                mod_feats[i] = "prevBIO=" + tag;
-            }
-            else
-            {
-                mod_feats[i] = feats[i];
-            }
-        }
-        
-        return mod_feats;
-        
-    }
-
-   private static int find_which_max(double[][] matrix, int col)
-   {
-       double val = 0.0;
-       int max_ix = 0;
-       
-       for(int i = 0; i < matrix.length; i++)
-       {
-           if(matrix[i][col] > val)
-           {
-               val = matrix[i][col];
-               max_ix = i;
-           }
-       }
-       
-       return max_ix;
-       
-   }
-   
-   private static void find_max_viterbi(ArrayList<double[]> matrix, double[][] v, int[][] p, int col)
-   {
-       int n_states = matrix.size();
-       double[] poss;
-       double[] max_probs = new double[n_states];
-       int[] max_ix = new int[n_states];
-       
-       
-       for(int i = 0; i < n_states; i++)
-       {
-           poss = matrix.get(i);
-           
-           for(int j = 0; j < n_states; j++)
-           {
-               if(poss[j] > max_probs[j])
-               {
-                    max_probs[j] = poss[j];
-                    max_ix[j] = i;
-               }
-           }
-       }
-       
-       //copy values into our v and p matrices
-       for(int i = 0 ; i < n_states; i++) {
-           v[i][col] = max_probs[i];
-           p[i][col] = max_ix[i];
-       } 
-       
-   }
-
-
-    //Decode 1 sentence
-    public static ArrayList<String> viterbi_decode0(GISModel model, ArrayList<String[]> feat_matrix)
-    {
-        
-        int n_states = model.getNumOutcomes();
-        int n_obs = feat_matrix.size(); 
-        
-        String[] mod_feats;
-        double[][] v = new double[n_states][n_obs]; //viterbi probabilities
-        int[][]    p = new int[n_states][n_obs]; //viterbi backpointer
-                 
-        //Initial probability
-        double[] estimates = model.eval(mod_prevBioFeat(feat_matrix.get(0),"null"));
-
-        for(int i = 0; i < n_states; i++)
-        {
-            v[i][0] = estimates[i];
-        }
-
-        //Recursive steps
-        for(int j = 1; j < n_obs; j++)
-        { //for each observation 
-            //create a matrix to hold possible values, one row per possible prior state
-            ArrayList<double[]> estimateMatrix = new ArrayList<double[]>(); 
-            
-            for(int i = 0; i < n_states; i++)
-            { //for each possible prior state: P(s|s', o)
-                double v_prev = v[i][j - 1];
-                mod_feats = mod_prevBioFeat(feat_matrix.get(j), model.getOutcome(i));
-                estimates = model.eval(mod_feats);
-                
-                //multiply by previous viterbi prob: P(s|s',o) * v_t-1(s')
-                for(int k = 0; k < n_states; k++) {
-                    estimates[k] = estimates[k] * v_prev;
-                }
-                //add to estimate matrix
-                estimateMatrix.add(estimates);
-            }
-            
-            //Find max for each possible state and store
-            find_max_viterbi(estimateMatrix, v, p, j);
-            
-        }
-        
-        //Decode
-        ArrayList<String> results = new ArrayList<String>();
-        int back_pointer = find_which_max(v, n_obs - 1); //find last state that maximizes probability
-        results.add(model.getOutcome(back_pointer));
-        
-        //trace back based on back pointer
-        for(int j = n_obs - 1; j > 0; j--){ //we already decoded last position
-            back_pointer = p[back_pointer][j];
-            results.add(model.getOutcome(back_pointer));
-        }
-        Collections.reverse(results); //reverse backpointer list to get in order
-        return results;
-        
-    }
-    
-    //decode an entire document using viterbi,calls viterbi_decode0 on each sentence separately
-    public static ArrayList<String> viterbi_decode(GISModel model, ArrayList<String[]> feat_matrix) {
-        ArrayList<String> results = new ArrayList<String>();
-        String[] word_features = null;
-        ArrayList<String[]> sentence_features = new ArrayList<String[]>();
-        
-        for(int i = 0; i < feat_matrix.size(); i++){
-            word_features = feat_matrix.get(i);
-            
-            if(word_features == null) {
-                //Decode 1 sentence, we reached a sentence boundary
-                results.addAll(viterbi_decode0(model, sentence_features)); 
-                results.add(null); //add an empty result to mark boundary
-                sentence_features = new ArrayList<String[]>();  //create new for next sentence to use
-            } else {
-                sentence_features.add(word_features);
-            }
-        }
-        //The loop above could miss a decode if there is no null at the end
-        if(sentence_features.size() != 0)
-        {
-            results.addAll(viterbi_decode0(model, sentence_features));
-        }
-        
-        return results;
-        
-    }
+   // private static int find_which_max(double[][] matrix, int col)
+//    {
+//        double val = 0.0;
+//        int max_ix = 0;
+//
+//        for(int i = 0; i < matrix.length; i++)
+//        {
+//            if(matrix[i][col] > val)
+//            {
+//                val = matrix[i][col];
+//                max_ix = i;
+//            }
+//        }
+//
+//        return max_ix;
+//
+//    }
+//
+//    private static void find_max_viterbi(ArrayList<double[]> matrix, double[][] v, int[][] p, int col)
+//    {
+//        int n_states = matrix.size();
+//        double[] poss;
+//        double[] max_probs = new double[n_states];
+//        int[] max_ix = new int[n_states];
+//
+//
+//        for(int i = 0; i < n_states; i++)
+//        {
+//            poss = matrix.get(i);
+//
+//            for(int j = 0; j < n_states; j++)
+//            {
+//                if(poss[j] > max_probs[j])
+//                {
+//                     max_probs[j] = poss[j];
+//                     max_ix[j] = i;
+//                }
+//            }
+//        }
+//
+//        //copy values into our v and p matrices
+//        for(int i = 0 ; i < n_states; i++) {
+//            v[i][col] = max_probs[i];
+//            p[i][col] = max_ix[i];
+//        }
+//
+//    }
+//
+//
+//     //Decode 1 sentence
+//     public static ArrayList<String> viterbi_decode0(GISModel model, ArrayList<String[]> feat_matrix)
+//     {
+//         int n_states = model.getNumOutcomes();
+//         int n_obs = feat_matrix.size();
+//
+//         double[][][] v = new double[n_obs][n_states][n_states]; //viterbi probabilities
+//         int[][][]    bp = new int[n_obs][n_states][n_states]; //viterbi backpointer
+//
+//         //Initial probability
+//
+//
+//         double[] estimates = model.eval(feat_matrix.get(0),"null"));
+//         String prev_tag = "START";
+//         String prev_tag2 = "START";
+//         String w_labels = model.getAllOutcomes();
+//         String u_labels =
+//
+//
+//         //Recursive steps
+//         for(int i = 0; i < n_obs; i++) { //for each observation
+//             ArrayList<String> features = new ArrayList<String[]>();
+//             features.addAll(feat_matrix.get(i));
+//             for(int v = 0; v < n_states; v++){ //tag at i
+//                 for(int u = 0; u < n_states; u++){ //tag at i - 1
+//                     features.add("tag_i-1=" + model.getOutcome(u));
+//                     double[] probs = new probs[n_states];
+//                     for(int w = 0; w < n_states; w++) {
+//                         //features for obs i
+//                         features.add("tag_i-2=" + model.getOutcome(w));
+//                         probs[w] = log(model.eval(features.toArray())) + v[j-1][w][u];
+//                         features.remove(features.size() - 1); //remove tag_i-2
+//                     }
+//                     v[j][u][v] = max(probs);
+//                     bp[j][u][v] = /* index of max */
+//                     features.remove(features.size() - 1); //remove tag_i-1
+//                 }
+//         }
+//
+//         for(int v = 0; v < n_states; v++){
+//             for(int u = 0; u < n_states; u++){
+//             //termination step
+//             }
+//         }
+//
+//         //Decode
+//         ArrayList<String> results = new ArrayList<String>();
+//         int back_pointer = find_which_max(v, n_obs - 1); //find last state that maximizes probability
+//         results.add(model.getOutcome(back_pointer));
+//
+//         //trace back based on back pointer
+//         for(int j = n_obs - 1; j > 0; j--){ //we already decoded last position
+//             back_pointer = p[back_pointer][j];
+//             results.add(model.getOutcome(back_pointer));
+//         }
+//         Collections.reverse(results); //reverse backpointer list to get in order
+//         return results;
+//
+//     }
+//
+//     //decode an entire document using viterbi,calls viterbi_decode0 on each sentence separately
+//     public static ArrayList<String> viterbi_decode(GISModel model, ArrayList<String[]> feat_matrix) {
+//         ArrayList<String> results = new ArrayList<String>();
+//         String[] word_features = null;
+//         ArrayList<String[]> sentence_features = new ArrayList<String[]>();
+//
+//         for(int i = 0; i < feat_matrix.size(); i++){
+//             word_features = feat_matrix.get(i);
+//
+//             if(word_features == null) {
+//                 //Decode 1 sentence, we reached a sentence boundary
+//                 results.addAll(viterbi_decode0(model, sentence_features));
+//                 results.add(null); //add an empty result to mark boundary
+//                 sentence_features = new ArrayList<String[]>();  //create new for next sentence to use
+//             } else {
+//                 sentence_features.add(word_features);
+//             }
+//         }
+//         //The loop above could miss a decode if there is no null at the end
+//         if(sentence_features.size() != 0)
+//         {
+//             results.addAll(viterbi_decode0(model, sentence_features));
+//         }
+//
+//         return results;
+//
+//     }
     
 
     public static void usage()
     {
-        System.out.println("MaxEndModelTest usage: <gis_model_file> <feature_txt_file> <-simple|-viterbi>");
+        System.out.println("MaxEndModelTest usage: <gis_model_file> <feature_txt_file> <result_file> <-greedy|-viterbi>");
     }
     
     public static void main(String[] argv)
     {
         //Test to make sure that command line options are ok
-        if(argv.length != 3)
+        if(argv.length != 4)
         {
-            MaxEntModelTest.usage();
+            MaxEntModelPredict.usage();
             System.exit(1);
         }
         
         String model_file_name = argv[0];
         String feat_file_name = argv[1];
+        String result_file_name = argv[2];
         
         //read in model
         GISModel model = null;
@@ -228,7 +226,7 @@ class MaxEntModelPredict {
         
         //read text file of features
         ArrayList<String[]> feature_matrix = new ArrayList<String[]>();
-        ArrayList<String> realized_tags = new ArrayList<String>();
+        
         try {
             FileReader fileReader = new FileReader(feat_file_name);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -238,18 +236,13 @@ class MaxEntModelPredict {
             {
                 if(line.trim().length() > 0 )
                 {
-                    String[] features_raw = line.split(" ");
-                    realized_tags.add(features_raw[features_raw.length - 1]); //save tag
-                    String[] features = new String[features_raw.length - 1];
-                    System.arraycopy(features_raw, 0, features, 0, features.length); //everything except the tag
+                    String[] features = line.split(" ");
                     feature_matrix.add(features);
                 }
                 else
                 {
                     feature_matrix.add(null); //add a marker for boundary
-                    realized_tags.add(null); //add a marker for boundary
                 }
-
             }
             
             bufferedReader.close();
@@ -263,38 +256,38 @@ class MaxEntModelPredict {
         }
         
         //model predictions
-        ArrayList<String> predicted_tags;
-        if(argv[2].equals("-simple"))
+        ArrayList<String> predicted_tags = new ArrayList<String>();
+        if(argv[3].equals("-greedy"))
          {
-            predicted_tags = simple_decode(model, feature_matrix); 
+            predicted_tags = greedy_decode(model, feature_matrix); 
+        } else if (argv[3].equals("-viterbi")){
+            System.out.println("viterbi nyi");
+            System.exit(1);
         } else {
-            predicted_tags = viterbi_decode(model, feature_matrix);
+            System.out.println("method " + argv[3] + " nyi");
+            System.exit(1);
         }
-    
-        //Model Evaluation
-        ArrayList<String> realized_boundaries = get_boundaries(realized_tags);
-        ArrayList<String> predicted_boundaries = get_boundaries(predicted_tags);
-        
-        double tag_accuracy = simple_accuracy(predicted_tags, realized_tags);
-        double boundary_precision = pct_in(predicted_boundaries, realized_boundaries);
-        double boundary_recall = pct_in(realized_boundaries, predicted_boundaries);
-        double boundary_f1 = 2 * ((boundary_precision * boundary_recall) / (boundary_precision + boundary_recall));
-        
-        //write out evaluation results to std out
-        System.out.println("Tag Accuracy: " + tag_accuracy);
-        System.out.println("Boundary Precision: " + boundary_precision);
-        System.out.println("Boundary Recall: " + boundary_recall);
-        System.out.println("Boundary F1: " + boundary_f1);
-        
-        //Write out tags to std out
-        for(String tag : predicted_tags)
-        {
-            if(tag != null) {
-                System.out.println(tag);
-            } else {
-                System.out.println(); //empty line
+            
+        //Write out tags to the file name provided
+        try {
+            PrintWriter writer = new PrintWriter(result_file_name, "UTF-8");
+
+            for(String tag : predicted_tags){
+                if(tag != null){
+                    writer.println(tag);
+                } else {
+                    writer.println();
+                }
             }
-        }    
+
+            writer.close();
+
+        } catch(IOException e) {
+            System.out.println("Error:" + e);
+            System.exit(1);
+        }
+
+            
     }
     
     
--- term_project/src/maxentmodel.py
+++ term_project/src/maxentmodel.py
@@ -28,55 +28,93 @@ class Maxentmodel:
         features["firstCaps"] = word[0].isupper()
         features["has-number"] = re.search(r'[0-9]', word) != None
         features["is-long-word"] = len(word) > self.LONGWORDLEN
-        features["tag_i-1"] = prev_tag
-        features["tag_i-2"] = prev_tag2
+        if prev_tag != None and prev_tag2 != None:
+            features["tag_i-1"] = prev_tag
+            features["tag_i-2"] = prev_tag2
         return features
             
-    #returns tuple of i -2 and i - 1 tags
-    def prev_tags(self, tags):
-        tlen = len(tags)
-        if tlen == 0:
-            prev_tag = prev_tag2 = self.START
-        elif tlen == 1:
-            prev_tag2 = self.START
-            prev_tag = tags[-1]
-        else:
-            prev_tag2, prev_tag = tags[-2:]
-        return prev_tag2, prev_tag
-        
-    def build_features(self, tagged_corpus):
+    def build_features_sent(self, sent):
         featureset = []
-        for sent in tagged_corpus:
-            tags = []
-            for word, tag in sent:
-                prev_tag2, prev_tag = self.prev_tags(tags)
-                features = self.feat_map(word, prev_tag, prev_tag2)
-                featureset.append(features)
-                tags.append(tag)
+        tags = [self.START, self.START]
+        prev_tag2 = prev_tag = None
+        for word, tag in sent:
+            if tag != None:
+                prev_tag2, prev_tag = tags[-2:]
+            features = self.feat_map(word, prev_tag, prev_tag2)
+            featureset.append(features)
+            tags.append(tag)
         return featureset
 
 
+    def features_str(self, features):
+        str = " ".join([feat + "=" + unicode(val) for feat,val in features.iteritems()])
+        return str.encode("utf-8")
+
+
     def train(self, tagged_corpus, model_path, iter = 100):
-        features = self.build_features(tagged_corpus)
-        tags = [tag for sent in tagged_corpus for word, tag in sent]
-        feat_file = tempfile.NamedTemporaryFile(mode="r+w", delete=False)
-        for i in xrange(len(features)):
-            str_feats = " ".join([feat + "=" + unicode(val) for feat,val in features[i].iteritems()])
-            feat_file.write(str_feats.encode("utf-8"))
-            feat_file.write(" " + tags[i])
+        feature_matrix = []
+        realized_tags = []
+        for sent in tagged_corpus:
+            for word,tag in sent:
+                realized_tags.append(tag) #collect realized tags
+            sent_features = self.build_features_sent(sent)
+            for word_features in sent_features:
+                feature_matrix.append(word_features)
+        feat_file = tempfile.NamedTemporaryFile(mode="r+w")
+        for i in xrange(len(feature_matrix)):
+            str_feats = self.features_str(feature_matrix[i])
+            feat_file.write(str_feats)
+            feat_file.write(" " + realized_tags[i])
             feat_file.write("\n")
+        feat_file.flush() #make sure it is all written out
         #run java trainer
         print "Calling java MaxEntModelTrain to train classifier, storing to %s" % model_path
         feat_file.seek(0)
         failure = subprocess.call(['java', '-cp', self.OPENNLPCLASSPATH, 'MaxEntModelTrain', feat_file.name, model_path])
         if not failure:
             self.model_path = model_path
+        feat_file.close()
         
+    
+    def read_results(self, file_handle):
+        file_handle.seek(0)
+        corpus_results = []
+        sent_results = []
+        for line in file_handle:
+            tag = line.rstrip('\n')
+            if len(tag) > 0:
+                sent_results.append(tag)
+            else:
+                #sentences are separated by blank line
+                corpus_results.append(sent_results)
+                sent_results = []
+        return corpus_results
+    
         
-    def tag_corpus(self, corpus):
-        #create features for corpus
-        #run java model
-        #read in results
-        #return in usual python structure
-        
+    def tag_corpus(self, corpus, method = "greedy"):
+        if self.model_path == None:
+            raise ValueError("Missing a model path, train!")
+            
+        feat_file = tempfile.NamedTemporaryFile(mode="r+w")
+        for sent in corpus:
+            fake_tags = [ None ] * len(sent)
+            sent_features = self.build_features_sent(zip(sent, fake_tags))
+            for word_features in sent_features:
+                str_feats = self.features_str(word_features)
+                feat_file.write(str_feats)
+                feat_file.write("\n") #new line for each word
+            feat_file.write("\n") #blank line after every sentence
+        feat_file.flush() #make sure it is all written out
+        result_file = tempfile.NamedTemporaryFile(mode="r+w")
+        print "Calling java MaxEntModelPredict to tag sentence"
+        failure = subprocess.call(['java', '-cp', self.OPENNLPCLASSPATH, 'MaxEntModelPredict', self.model_path, feat_file.name, result_file.name, "-" + method])
+        results = []
+        if not failure:
+            corpus_tags = self.read_results(result_file)
+            for i in xrange(len(corpus)):
+                results.append(zip(corpus[i], corpus_tags[i]))
+            feat_file.close()
+            result_file.close()
+        return results
+
     
