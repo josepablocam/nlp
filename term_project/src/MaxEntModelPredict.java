02698b100216510d5c3f44dc0ccddb36888025be
Jose Cambronero <josepablocam@gmail.com>
2015-04-27 23:45:32 -0400

    first working version of viterbi, need to add prune

--- term_project/src/MaxEntModelPredict.java
+++ term_project/src/MaxEntModelPredict.java
@@ -56,142 +56,150 @@ class MaxEntModelPredict {
     }
     
     
-   // private static int find_which_max(double[][] matrix, int col)
-//    {
-//        double val = 0.0;
-//        int max_ix = 0;
-//
-//        for(int i = 0; i < matrix.length; i++)
-//        {
-//            if(matrix[i][col] > val)
-//            {
-//                val = matrix[i][col];
-//                max_ix = i;
-//            }
-//        }
-//
-//        return max_ix;
-//
-//    }
-//
-//    private static void find_max_viterbi(ArrayList<double[]> matrix, double[][] v, int[][] p, int col)
-//    {
-//        int n_states = matrix.size();
-//        double[] poss;
-//        double[] max_probs = new double[n_states];
-//        int[] max_ix = new int[n_states];
-//
-//
-//        for(int i = 0; i < n_states; i++)
-//        {
-//            poss = matrix.get(i);
-//
-//            for(int j = 0; j < n_states; j++)
-//            {
-//                if(poss[j] > max_probs[j])
-//                {
-//                     max_probs[j] = poss[j];
-//                     max_ix[j] = i;
-//                }
-//            }
-//        }
-//
-//        //copy values into our v and p matrices
-//        for(int i = 0 ; i < n_states; i++) {
-//            v[i][col] = max_probs[i];
-//            p[i][col] = max_ix[i];
-//        }
-//
-//    }
-//
-//
-//     //Decode 1 sentence
-//     public static ArrayList<String> viterbi_decode0(GISModel model, ArrayList<String[]> feat_matrix)
-//     {
-//         int n_states = model.getNumOutcomes();
-//         int n_obs = feat_matrix.size();
-//
-//         double[][][] v = new double[n_obs][n_states][n_states]; //viterbi probabilities
-//         int[][][]    bp = new int[n_obs][n_states][n_states]; //viterbi backpointer
-//
-//         //Initial probability
-//
-//
-//         double[] estimates = model.eval(feat_matrix.get(0),"null"));
-//         String prev_tag = "START";
-//         String prev_tag2 = "START";
-//         String w_labels = model.getAllOutcomes();
-//         String u_labels =
-//
-//
-//         //Recursive steps
-//         for(int i = 0; i < n_obs; i++) { //for each observation
-//             ArrayList<String> features = new ArrayList<String[]>();
-//             features.addAll(feat_matrix.get(i));
-//             for(int v = 0; v < n_states; v++){ //tag at i
-//                 for(int u = 0; u < n_states; u++){ //tag at i - 1
-//                     features.add("tag_i-1=" + model.getOutcome(u));
-//                     double[] probs = new probs[n_states];
-//                     for(int w = 0; w < n_states; w++) {
-//                         //features for obs i
-//                         features.add("tag_i-2=" + model.getOutcome(w));
-//                         probs[w] = log(model.eval(features.toArray())) + v[j-1][w][u];
-//                         features.remove(features.size() - 1); //remove tag_i-2
-//                     }
-//                     v[j][u][v] = max(probs);
-//                     bp[j][u][v] = /* index of max */
-//                     features.remove(features.size() - 1); //remove tag_i-1
-//                 }
-//         }
-//
-//         for(int v = 0; v < n_states; v++){
-//             for(int u = 0; u < n_states; u++){
-//             //termination step
-//             }
-//         }
-//
-//         //Decode
-//         ArrayList<String> results = new ArrayList<String>();
-//         int back_pointer = find_which_max(v, n_obs - 1); //find last state that maximizes probability
-//         results.add(model.getOutcome(back_pointer));
-//
-//         //trace back based on back pointer
-//         for(int j = n_obs - 1; j > 0; j--){ //we already decoded last position
-//             back_pointer = p[back_pointer][j];
-//             results.add(model.getOutcome(back_pointer));
-//         }
-//         Collections.reverse(results); //reverse backpointer list to get in order
-//         return results;
-//
-//     }
-//
-//     //decode an entire document using viterbi,calls viterbi_decode0 on each sentence separately
-//     public static ArrayList<String> viterbi_decode(GISModel model, ArrayList<String[]> feat_matrix) {
-//         ArrayList<String> results = new ArrayList<String>();
-//         String[] word_features = null;
-//         ArrayList<String[]> sentence_features = new ArrayList<String[]>();
-//
-//         for(int i = 0; i < feat_matrix.size(); i++){
-//             word_features = feat_matrix.get(i);
-//
-//             if(word_features == null) {
-//                 //Decode 1 sentence, we reached a sentence boundary
-//                 results.addAll(viterbi_decode0(model, sentence_features));
-//                 results.add(null); //add an empty result to mark boundary
-//                 sentence_features = new ArrayList<String[]>();  //create new for next sentence to use
-//             } else {
-//                 sentence_features.add(word_features);
-//             }
-//         }
-//         //The loop above could miss a decode if there is no null at the end
-//         if(sentence_features.size() != 0)
-//         {
-//             results.addAll(viterbi_decode0(model, sentence_features));
-//         }
-//
-//         return results;
-//
-//     }
+   private static double max(double[] probs){
+       double max_val = 0.0;
+
+       for(int i = 0; i < probs.length; i++){
+           if(probs[i] > max_val){
+               max_val = probs[i];
+           }
+       }
+
+       return max_val;
+   }
+   
+   private static int which_max(double[] probs){
+       double max_val = 0.0;
+       int max_ix = 0;
+       
+       for(int i = 0; i < probs.length; i++){
+           if(probs[i] > max_val){
+               max_val = probs[i];
+               max_ix = i;
+           }
+       }
+       
+       return max_ix;
+   }
+
+    //Decode 1 sentence using viterbi method
+    public static ArrayList<String> viterbi_decode0(GISModel model, ArrayList<String[]> feat_matrix){
+        int n_states = model.getNumOutcomes();
+        int n_obs = feat_matrix.size();
+        int n_feats = feat_matrix.get(0).length;
+
+        double[][][] pi = new double[n_obs][n_states + 1][n_states + 1]; //viterbi probabilities, add 1 state for START
+        int[][][]    bp = new int[n_obs][n_states + 1][n_states + 1]; //viterbi backpointer, add 1 state for START
+        
+        String[] labels = new String[n_states + 1];
+        labels[labels.length - 1] = "START"; //put start special symbol at end
+        
+        for(int i = 0; i < labels.length - 1; i++) {
+            labels[i] = model.getOutcome(i); //fill in rest of labels
+        }
+        
+        String[] raw_feats = feat_matrix.get(0); //features for first word in sentence
+        String[] ext_feats = new String[n_feats + 2]; //add 2 spaces for previous 2 tags
+        System.arraycopy(raw_feats, 0, ext_feats, 0, n_feats);
+        
+        ext_feats[n_feats] = "tag_i-2=START";
+        ext_feats[n_feats + 1] = "tag_i-1=START";
+        
+        //initial probabilities
+        System.out.println("Initializing probabilities");
+        double[] init_probs = model.eval(ext_feats);
+        for(int v = 0; v < n_states; v++) {
+            pi[0][n_states][v] = init_probs[v]; //v[0]['START'][v]
+            bp[0][n_states][v] = n_states; //bp[0]['START'][v] = 'START'
+        }
+        
+        //recursive step
+        //TODO: implement prune        
+        System.out.println("Recursive step");
+        for(int i = 1; i < n_obs; i++) { //for each observation
+            System.out.println("index:" + i);
+            System.arraycopy(feat_matrix.get(i), 0, ext_feats, 0, n_feats);
+            for(int v = 0; v < n_states; v++){ //tag at i
+                for(int u = 0; u < n_states; u++){ //tag at i - 1
+                    ext_feats[n_feats + 1] = "tag_i-1=" + labels[u];
+                    int w_lim = (i == 1) ? n_states + 1 : n_states; //+1 to allow for 'START' when i = 1
+                    double[] probs = new double[w_lim];
+                    for(int w = 0; w < w_lim; w++) {
+                        ext_feats[n_feats] = "tag_i-2=" + labels[w];
+                        if(pi[i-1][w][u] > 0){
+                            probs[w] = model.eval(ext_feats)[v] * pi[i - 1][w][u];
+                        }
+                    }
+                    pi[i][u][v] = max(probs);
+                    bp[i][u][v] = which_max(probs);
+                }
+            }
+        }
+    
+
+        //Decode
+        System.out.println("decoding");
+        ArrayList<String> results = new ArrayList<String>();
+        
+        double max_prob = 0.0;
+        int pred_w = 0, pred_u = 0, pred_v = 0;
+        
+        //find last 2 states
+        for(int v = 0; v < n_states; v++){
+            for(int u = 0; u < n_states; u++){
+                if(pi[n_obs - 1][u][v] > max_prob){
+                    max_prob = pi[n_obs - 1][u][v];
+                    pred_u = u;
+                    pred_v = v;
+                }
+            }
+        }
+        
+        results.add(model.getOutcome(pred_v));
+        results.add(model.getOutcome(pred_u));
+        
+        //trace back based on back pointer
+        for(int j = n_obs - 1; j >= 2; j--){ //we already decoded last position
+            pred_w = bp[j][pred_u][pred_v];
+            results.add(model.getOutcome(pred_w));
+            pred_v = pred_u;
+            pred_u = pred_w;
+        }
+        Collections.reverse(results); //reverse backpointer list to get in order
+        for(String tag: results) {
+            System.out.println(tag);
+        }
+        
+        
+        return results;
+    }
+
+    
+    //decode an entire document using viterbi,calls viterbi_decode0 on each sentence
+    public static ArrayList<String> viterbi_decode(GISModel model, ArrayList<String[]> feat_matrix) {
+        ArrayList<String> results = new ArrayList<String>();
+        String[] word_features = null;
+        ArrayList<String[]> sentence_features = new ArrayList<String[]>();
+
+        for(int i = 0; i < feat_matrix.size(); i++){
+            word_features = feat_matrix.get(i);
+
+            if(word_features == null) {
+                //Decode 1 sentence, we reached a sentence boundary
+                results.addAll(viterbi_decode0(model, sentence_features));
+                results.add(null); //add an empty result to mark boundary
+                sentence_features = new ArrayList<String[]>();  //create new for next sentence to use
+            } else {
+                sentence_features.add(word_features);
+            }
+        }
+        //The loop above could miss a decode if there is no null at the end
+        if(sentence_features.size() != 0) {
+            results.addAll(viterbi_decode0(model, sentence_features));
+        }
+        
+        return results;
+    }
     
 
     public static void usage()
@@ -261,8 +269,7 @@ class MaxEntModelPredict {
          {
             predicted_tags = greedy_decode(model, feature_matrix); 
         } else if (argv[3].equals("-viterbi")){
-            System.out.println("viterbi nyi");
-            System.exit(1);
+            predicted_tags = viterbi_decode(model, feature_matrix);
         } else {
             System.out.println("method " + argv[3] + " nyi");
             System.exit(1);
